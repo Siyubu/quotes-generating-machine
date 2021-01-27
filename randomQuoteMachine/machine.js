881b9b1eac08fd86675b2484e084ecf4c1f080d1
siyubu <solangeiyubu@gmail.com>
2021-01-27 12:10:27 +0200

    quotes random machine

---
+++ randomQuoteMachine/machine.js
@@ -0,0 +1,47 @@
+
+const quotes = [
+    {
+        "quote": "Forget all the reasons why it won't work and believe the one reason why it will",
+        "author": "Unknown"
+    },
+    {
+        "quote": "Always do what you are afraid to do",
+        "author": "Ralph Waldo Emerson"
+    },
+
+    {
+        "quote": "Don’t be intimidated by what you don’t know. That can be your greatest strength and ensure that you do things differently from everyone else.",
+        "author": "Sara Blakely"
+    },
+    {
+        "quote": "If you keep on doing what you've always done, you will keep getting what you've always gotten.",
+        "author": "Unknown"
+    },
+    {
+        "quote": " For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. John 3:16",
+        "author": "Jesus Christ"
+    },
+    {
+        "quote": "The surest way to find your dream job is to create it.",
+        "author": "Unknown"
+
+    }
+];
+
+window.onload= init
+function init(){
+    generateQuote();  
+}
+function generateQuote(){
+    let twitterLink="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22"
+    let num = Math.floor(Math.random() * quotes.length)
+    let newQuote = quotes[num];
+    let quoteLinkFormat=newQuote.quote.replaceAll(" ","%20")
+    twitterLink+=quoteLinkFormat +"%22";
+    let authorLinkFormat=newQuote.author.replaceAll(" ","%20")
+    twitterLink+='-'+ authorLinkFormat
+
+    $('#text').html(newQuote.quote);
+    $('#author').html(newQuote.author);
+    $('#tweet-quote').attr("href",twitterLink)
+}
